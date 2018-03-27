ce216e39cb8d48d7442df690954bb01e63830d23
Filip Fornmark <filip.fornmark@pluspole.se>
2018-03-27 16:59:39 +0200

    test socket server

--- test-things/socket_server.py
+++ test-things/socket_server.py
@@ -1,4 +1,5 @@
 import socket
+import random
 
 
 s = socket.socket()
@@ -7,15 +8,39 @@ port = 12345
 print(host, port)
 s.bind((host, port))
 
-s.listen(5)
+s.listen(1)
 c, addr = s.accept()
 print('Got connection from', addr)
-c.send(b'Thank you for connecting\n')
-while True:
+c.send(b'Thank you for connecting. Let\'s play!\n')
 
+responses = {
+    'You fight like a dairy farmer\n':
+        'how appropriate! You fight like a cow!',
+    'Every word you say to me is stupid\n':
+        'i wanted to make sure you\'d feel comfortable with me.',
+    'My name is feared in every dirty corner of this island!\n':
+        'so you got that job as a janitor, after all.'
+}
+scores = [0, 0]
+
+while True:
+    if scores[0] == 3:
+        c.sendall(b'I win!\n')
+        c.close()
+
+    elif scores[1] == 3:
+        c.sendall(b'You win!\n')
+        c.close()
+
+    q, a = random.choice(list(responses.items()))
+    c.sendall(q.encode())
     data = c.recv(1024)
     if not data:
         break
 
-    resp = b'Echo: ' + data + b'\n'
-    c.sendall(resp)
+    data = data.decode().strip().lower()
+
+    if data == a:
+        scores[1] += 1
+    else:
+        scores[0] += 1
