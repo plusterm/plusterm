1ef6866820a6297912a59cc097626538865f78cd
Filip Fornmark <filip.fornmark@pluspole.se>
2018-04-19 09:56:38 +0200

    slider plotter; bugfix!

--- modules/plotter_adv/plotter_adv.py
+++ modules/plotter_adv/plotter_adv.py
@@ -7,6 +7,8 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg
 
+import wx.lib.inspection
+
 
 class Plotter_adv(wx.Frame):
     def __init__(self, parent, title):
@@ -21,7 +23,6 @@ class Plotter_adv(wx.Frame):
         self.group_role = []
 
         self.Bind(wx.EVT_CLOSE, self.on_close)
-
         self.init_ui()
 
     def init_ui(self):
@@ -35,19 +36,21 @@ class Plotter_adv(wx.Frame):
         self.regex_panel = wx.Panel(self)
         self.regex_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        preview_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        preview_label = wx.StaticText(self.regex_panel, label='Preview: ')
-        self.preview_regex = wx.StaticText(self.regex_panel)
-
-        preview_sizer.Add(preview_label)
-        preview_sizer.Add(self.preview_regex)
-
-        self.regex_sizer.Add(preview_sizer)
-
         self.regex_tb = wx.TextCtrl(
             self.regex_panel,
             style=wx.TE_PROCESS_ENTER,
             size=(400, 23))
+
+        regex_font = wx.Font(
+            10, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u'Consolas')
+        self.regex_tb.SetFont(regex_font)
+
+        preview_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        preview_label = wx.StaticText(self.regex_panel, label='Preview: ')
+        self.preview_regex = wx.StaticText(self.regex_panel)
+
+        preview_sizer.Add(preview_label, 1, wx.LEFT, 5)
+        preview_sizer.Add(self.preview_regex, 1, wx.LEFT, 5)
 
         self.regex_group_sizer = wx.BoxSizer(wx.VERTICAL)
         self.choices = [
@@ -60,33 +63,50 @@ class Plotter_adv(wx.Frame):
             self.regex_panel, choices=self.choices, style=wx.CB_READONLY)
         group_dd1.Bind(wx.EVT_COMBOBOX, self.generate_group_role)
         group_dd1.SetSelection(0)
-        self.regex_group_sizer.Add(group_label1)
-        self.regex_group_sizer.Add(group_dd1)
+        self.regex_group_sizer.Add(group_label1, 0, wx.LEFT, 5)
+        self.regex_group_sizer.Add(group_dd1, 0, wx.LEFT, 5)
+        self.regex_group_sizer.AddSpacer(5)
 
         group_label2 = wx.StaticText(self.regex_panel, label='Group 2')
         group_dd2 = wx.ComboBox(
             self.regex_panel, choices=self.choices, style=wx.CB_READONLY)
         group_dd2.Bind(wx.EVT_COMBOBOX, self.generate_group_role)
         group_dd2.SetSelection(2)
-        self.regex_group_sizer.Add(group_label2)
-        self.regex_group_sizer.Add(group_dd2)
+        self.regex_group_sizer.Add(group_label2, 0, wx.LEFT, 5)
+        self.regex_group_sizer.Add(group_dd2, 0, wx.LEFT, 5)
+        self.regex_group_sizer.AddSpacer(5)
 
         group_label3 = wx.StaticText(self.regex_panel, label='Group 3')
         group_dd3 = wx.ComboBox(
             self.regex_panel, choices=self.choices, style=wx.CB_READONLY)
         group_dd3.Bind(wx.EVT_COMBOBOX, self.generate_group_role)
         group_dd3.SetSelection(1)
-        self.regex_group_sizer.Add(group_label3)
-        self.regex_group_sizer.Add(group_dd3)
+        self.regex_group_sizer.Add(group_label3, 0, wx.LEFT, 5)
+        self.regex_group_sizer.Add(group_dd3, 0, wx.LEFT, 5)
+        self.regex_group_sizer.AddSpacer(5)
 
         self.regex_tb.Bind(wx.EVT_TEXT_ENTER, self.on_enter_regex)
         self.regex_tb.SetValue(self.regex)
+
         self.generate_group_role(wx.EVT_TEXT_ENTER)
 
+        self.static_line1 = wx.StaticLine(
+            self.regex_panel, wx.ID_ANY, style=wx.LI_HORIZONTAL)
+        self.static_line2 = wx.StaticLine(
+            self.regex_panel, wx.ID_ANY, style=wx.LI_HORIZONTAL)
+        self.static_line3 = wx.StaticLine(
+            self.regex_panel, wx.ID_ANY, style=wx.LI_HORIZONTAL)
+
+        # Add to regex sizer
         self.regex_sizer.Add(self.regex_tb)
+        self.regex_sizer.Add(self.static_line1, 0, wx.ALL | wx.EXPAND, 5)
+        self.regex_sizer.Add(preview_sizer)
+        self.regex_sizer.Add(self.static_line2, 0, wx.ALL | wx.EXPAND, 5)
         self.regex_sizer.Add(self.regex_group_sizer)
+        self.regex_sizer.Add(self.static_line3, 0, wx.ALL | wx.EXPAND, 5)
         self.regex_panel.SetSizer(self.regex_sizer)
 
+        # Plot settings panel
         self.panel = wx.Panel(self)
 
         nplot_label = wx.StaticText(self.panel, label='Number of plots: ')
@@ -112,6 +132,7 @@ class Plotter_adv(wx.Frame):
 
         self.SetBackgroundColour('lightgray')
         self.SetSizer(self.mainSizer)
+        self.SetMinSize(self.GetSize())
         self.regex_panel.Hide()
         self.Centre()
         self.Show(True)
@@ -119,14 +140,21 @@ class Plotter_adv(wx.Frame):
     def toggle_adv_settings(self, event):
         if not self.regex_panel.IsShown():
             self.regex_panel.Show()
+            self.static_line1.Show()
+            self.static_line2.Show()
+            self.static_line3.Show()
             self.Layout()
             self.Fit()
             self.Update()
+            self.SetMinSize(self.GetSize())
         else:
             self.regex_panel.Hide()
+            self.static_line1.Hide()
+            self.static_line2.Hide()
+            self.static_line3.Hide()
             self.Layout()
-            # self.Fit()
             self.Update()
+            self.SetMinSize(self.GetSize())
 
     def clear_regex_panel(self):
         nr = len(self.regex_group_sizer.GetChildren()) - 1
@@ -136,6 +164,8 @@ class Plotter_adv(wx.Frame):
         for i in range(nr, -1, -1):
             self.regex_group_sizer.Hide(index=i)
             self.regex_group_sizer.Remove(index=i)
+
+        self.SetMinSize(self.GetSize())
 
     def on_enter_regex(self, event):
         self.clear_regex_panel()
@@ -164,12 +194,14 @@ class Plotter_adv(wx.Frame):
                     choices=self.choices,
                     style=wx.CB_READONLY)
                 group_dd.Bind(wx.EVT_COMBOBOX, self.generate_group_role)
-                self.regex_group_sizer.Add(group_label)
-                self.regex_group_sizer.Add(group_dd)
+                self.regex_group_sizer.Add(group_label, 1, wx.LEFT, 5)
+                self.regex_group_sizer.Add(group_dd, 1, wx.LEFT, 5)
+                self.regex_group_sizer.AddSpacer(5)
 
             self.regex_panel.Update()
             self.Layout()
             self.Fit()
+            self.SetMinSize(self.GetSize())
 
     def get_params(self, data):
         a = self.cregex.findall(data[1].decode(errors='ignore').strip())
@@ -196,21 +228,21 @@ class Plotter_adv(wx.Frame):
         for i in range(n):
             self.nplot_sizerlist.append(wx.BoxSizer(wx.HORIZONTAL))
             self.nplot_sizerlist[i].Add(
-                wx.StaticText(self.panel, label=' x: '))
+                wx.StaticText(self.panel, label='x:'), 0, wx.LEFT, 5)
             self.nplot_sizerlist[i].Add(
                 wx.ComboBox(
                     self.panel,
                     style=wx.CB_READONLY,
-                    choices=self.params))
+                    choices=self.params), 0, wx.LEFT, 5)
             self.nplot_sizerlist[i].Add(
                 wx.StaticText(
                     self.panel,
-                    label='  y: '))
+                    label='y:'), 0, wx.LEFT, 5)
             self.nplot_sizerlist[i].Add(
                 wx.ComboBox(
                     self.panel,
                     style=wx.CB_READONLY,
-                    choices=self.params[1:]))
+                    choices=self.params[1:]), 0, wx.LEFT, 5)
             self.plot_set_sizer.Add(self.nplot_sizerlist[i])
 
         self.applybutton = wx.Button(self.panel, label='Plot')
@@ -224,6 +256,11 @@ class Plotter_adv(wx.Frame):
         self.panel.Update()
         self.Layout()
         self.SetSizer(self.mainSizer)
+
+        if self.regex_panel.IsShown():
+            self.Fit()
+
+        self.SetMinSize(self.GetSize())
 
     def generate_group_role(self, event):
         self.params = ['time']
@@ -267,7 +304,7 @@ class Plotwindow(wx.Frame):
         super(Plotwindow, self).__init__(
             parent,
             title=title,
-            size=(640, 480))
+            size=(600, 800))
         pub.subscribe(self.plot_data, 'serial.data')
 
         self.nplots = len(settings.nplot_sizerlist)
@@ -275,6 +312,10 @@ class Plotwindow(wx.Frame):
         self.index_warned = False
         self.xdata = []
         self.ydata = []
+
+        self.index_window = 100
+        self.end_index = self.index_window
+        self.start_index = 0
 
         for i in range(self.nplots):
             self.xdata.append([])
@@ -303,7 +344,7 @@ class Plotwindow(wx.Frame):
         self.SetMenuBar(menubar)
         self.Bind(wx.EVT_MENU_OPEN, self.save_csv)
 
-        self.figure = Figure(dpi=75)
+        self.figure = Figure(dpi=100)
 
         if self.nplots == 1:
             self.axes = []
@@ -312,16 +353,58 @@ class Plotwindow(wx.Frame):
         else:
             self.axes = self.figure.subplots(self.nplots, 1)
 
+        mainsizer = wx.BoxSizer()
         self.canvasPanel = wx.Panel(self)
         self.canvas = FigureCanvas(self.canvasPanel, wx.ID_ANY, self.figure)
         self.canvasSizer = wx.BoxSizer(wx.VERTICAL)
         self.canvasSizer.Add(self.canvas, 1, flag=wx.ALL | wx.EXPAND)
 
+        self.toolbar_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.canvas_sb = wx.Slider(
+            self.canvasPanel,
+            wx.ID_ANY,
+            minValue=0)
+
+        dp_lab = wx.StaticText(
+            self.canvasPanel,
+            label='Datapoints')
+
+        self.dp_tot_lab = wx.StaticText(self.canvasPanel, label=' / 0 (max)')
+        self.dp_txt = wx.TextCtrl(
+            self.canvasPanel, size=(40, -1), style=wx.TE_PROCESS_ENTER)
+
+        self.dp_txt.SetValue(str(self.index_window))
+
+        reset_btn = wx.Button(self.canvasPanel, label='Reset')
+        reset_btn.Bind(wx.EVT_BUTTON, self.on_reset)
+
+        self.dp_txt.Bind(wx.EVT_TEXT_ENTER, self.on_enter_nrdp)
+        self.canvas_sb.Bind(wx.EVT_SCROLL, self.on_scroll_event)
+
         self.canvas_toolbar = NavigationToolbar2WxAgg(self.canvas)
         self.canvas_toolbar.Realize()
-        self.canvasSizer.Add(self.canvas_toolbar, 0, wx.ALL | wx.EXPAND)
         self.canvas_toolbar.Update()
+
+        self.toolbar_sizer.Add(self.canvas_toolbar, 0)
+        self.toolbar_sizer.Add(
+            dp_lab, 0, wx.LEFT | wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT, 5)
+
+        self.toolbar_sizer.Add(
+            self.dp_txt,
+            0,
+            wx.LEFT | wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_RIGHT,
+            5)
+
+        self.toolbar_sizer.Add(self.dp_tot_lab, 0, wx.ALIGN_CENTER_VERTICAL, 5)
+        self.toolbar_sizer.Add(
+            reset_btn, 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT, 20)
+
+        self.canvasSizer.Add(self.canvas_sb, 0, wx.EXPAND)
+        self.canvasSizer.Add(self.toolbar_sizer, 0, wx.EXPAND)
         self.canvasPanel.SetSizer(self.canvasSizer)
+        mainsizer.Add(self.canvasPanel, 1, wx.EXPAND)
+        self.SetSizer(mainsizer)
+
         # set t0
         self.t0 = time.time()
         self.Show()
@@ -349,8 +432,7 @@ class Plotwindow(wx.Frame):
                             param_val_pair.append((param, val))
 
                 px, py = self.selected_params[i]
-                xfound = False
-                yfound = False
+                xfound, yfound = False, False
                 found_vals = {}
                 for (param, val) in param_val_pair:
                     if not yfound and param == py and val != '':
@@ -369,11 +451,7 @@ class Plotwindow(wx.Frame):
                     if yfound and xfound:
                         self.xdata[i].append(found_vals['x'])
                         self.ydata[i].append(found_vals['y'])
-
-                self.axes[i].clear()
-                self.axes[i].plot(self.xdata[i], self.ydata[i], '.-')
-                self.axes[i].set_xlabel(px)
-                self.axes[i].set_ylabel(py)
+                        break
 
             except IndexError as e:
                 if not self.index_warned:
@@ -386,11 +464,53 @@ class Plotwindow(wx.Frame):
                     dlg.ShowModal()
                     del dlg
 
-        try:
-            self.canvas.draw()
+        max_len = max([len(x) for x in self.xdata])
+        self.canvas_sb.SetMax(max_len - self.index_window)
+        self.dp_tot_lab.SetLabel(' / ' + str(max_len) + ' (max)')
+        self.start_index = max_len - self.index_window
+        self.end_index = max_len
+        self.draw_plot()
 
-        except Exception:
+    def draw_plot(self, scroll_event=False):
+        for i, ax in enumerate(self.axes):
+            ax.clear()
+            current_x = self.xdata[i][self.start_index:self.end_index]
+            current_y = self.ydata[i][self.start_index:self.end_index]
+            ax.plot(current_x, current_y, '.-')
+
+            x_lab, y_lab = self.selected_params[i]
+            ax.set_xlabel(x_lab)
+            ax.set_ylabel(y_lab)
+
+            if scroll_event is True and len(current_x) >= self.index_window:
+                ax.set_xlim(min(current_x), max(current_x))
+
+        self.canvas.draw()
+
+    def on_scroll_event(self, event):
+        self.start_index = event.GetPosition()
+        self.end_index = self.start_index + self.index_window
+
+        try:
+            self.draw_plot(scroll_event=True)
+
+        except IndexError:
             pass
+
+    def on_reset(self, event):
+        self.start_index = 0
+        self.end_index = max([len(x) for x in self.xdata])
+        self.draw_plot()
+
+    def on_enter_nrdp(self, event):
+        try:
+            dp = int(self.dp_txt.GetValue())
+
+        except AttributeError:
+            pass
+
+        else:
+            self.index_window = dp
 
     def save_csv(self, event):
         fd = wx.FileDialog(
@@ -409,9 +529,8 @@ class Plotwindow(wx.Frame):
                 header = 'plot_nr, x, y\n'
                 file.write(header)
                 for i, ax in enumerate(self.axes):
-                    line = ax.lines[0]
-                    x_data = line.get_xdata()
-                    y_data = line.get_ydata()
+                    x_data = self.xdata[i][self.start_index:self.end_index]
+                    y_data = self.ydata[i][self.start_index:self.end_index]
                     for x, y in zip(x_data, y_data):
                         data = '{}, {}, {}\n'.format(i + 1, x, y)
                         file.write(data)
